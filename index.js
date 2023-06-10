3d71ddff793a1fb3372b59451435637a698e2b29
kanishk44 <ks468@njit.edu>
2023-06-10 16:39:34 +0000

    Firest commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+const multer = require('multer')
+
+const upload = multer()
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  if(!req.file){
+    res.send("No file uploaded")
+    return
+  }
+  
+  const name = req.file.originalname
+  const type = req.file.mimetype
+  const size = req.file.size
+
+  res.json({
+    name,
+    type,
+    size
+  })
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
